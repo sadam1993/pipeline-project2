d913645c3a36ebb9d7d0d0870358271af052a163
sadam1993 <113791386+sadam1993@users.noreply.github.com>
2022-12-26 22:02:04 -0600

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,1 +1,13 @@
 print('My first python job  within Jenkinsfile.')
+pipeline {
+    agent any
+    stages {
+        stage('run') {
+            steps {
+                echo 'Welcome to Jenkins World'
+                sh 'python --version'
+                sh 'python pipeline.py'
+            }
+        }
+    }
+}
